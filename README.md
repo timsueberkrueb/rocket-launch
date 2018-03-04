9c7a13eb617e54fcea1c27e93b8b577b4dd0a247
Tim Süberkrüb <dev@timsueberkrueb.io>
2018-03-04 16:57:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Rocket Launch
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+[![GitHub release](https://img.shields.io/github/release/tim-sueberkrueb/rocket-launch.svg)](https://github.com/tim-sueberkrueb/rocket-launch/releases)
+[![GitHub issues](https://img.shields.io/github/issues/tim-sueberkrueb/rocket-launch.svg)](https://github.com/tim-sueberkrueb/rocket-launch/issues)
+[![Maintained](https://img.shields.io/maintenance/yes/2018.svg)](https://github.com/tim-sueberkrueb/rocket-launch/commits/develop)
+
+Watches a Cargo project for changes and automatically relaunches `cargo run`.
+
+`rocket-launch` will only restart `cargo run` when the compilation succeeds,
+thus keeping the application running. This is especially useful for writing
+server applications in Rust.
+
+It also only triggers a rebuild when a change in a file with a whitelisted extension happens.
+
+This tool is also pretty minimalistic (`< 200 lines`).
+Please use use [cargo watch](https://github.com/passcod/cargo-watch) for more options.
+
+## Dependencies
+
+[Rust](https://www.rust-lang.org) and the following crates and their dependencies are required:
+
+ * [clap](https://github.com/kbknapp/clap-rs)
+ * [notify](https://github.com/passcod/notify)
+
+## Installation
+
+Run:
+
+```bash
+cargo install
+```
+
+## Usage
+
+Run:
+
+```bash
+rocket-launch <path-to-cargo-project>
+```
+
+See `rocket-launch --help` for more options.
+
+## Licensing
+
+Licensed unter the terms of the MIT license.
---
+++ src/main.rs
@@ -0,0 +1,168 @@
+extern crate notify;
+extern crate clap;
+
+use std::sync::mpsc::channel;
+use std::time::Duration;
+use std::path::PathBuf;
+use std::fs::metadata;
+use std::env;
+use std::process::{Command, Child, exit};
+
+use notify::{Watcher, RecursiveMode, DebouncedEvent, watcher};
+use clap::{App, Arg};
+
+// Files with those extensions trigger a relaunch
+static WHITELISTED_EXTENSIONS: &'static [&'static str] = &[
+    ".rs",      // Rust source file
+    ".toml",    // TOML files (e.g. Cargo.toml)
+    ".tera",    // Tera templates
+    ".hbs",     // Handlebars templates
+    ".html",    // HTML files
+    ".js",      // JavaScript files
+];
+
+// Default interval to check for changes (in seconds)
+const DEFAULT_CHECK_INTERVAL: f32 = 0.5;
+
+fn is_whitelisted(path: PathBuf) -> bool {
+    let path_str = path.to_str().unwrap_or("");
+    WHITELISTED_EXTENSIONS.iter().any(|ext| path_str.ends_with(ext))
+}
+
+fn lift_cargo(project_path: &str, args: &[&str]) -> Child {
+    println!("⚙ Running cargo {}", args.join(" "));
+    // Filter out rustup environment variables to prevent interference
+    // in case rocket launch itself is running under cargo:
+    let child_env: Vec<(std::ffi::OsString, std::ffi::OsString)> = env::vars_os()
+        .filter(|&(ref key, _)| {
+            !key.to_str().unwrap_or("").starts_with("RUSTUP")
+        }).collect();
+
+    Command::new("cargo")
+        .current_dir(project_path)
+        .env_clear()
+        .envs(child_env)
+        .args(args)
+        .spawn()
+        .expect("Error creating process")
+}
+
+fn relaunch_cargo(project_path: &str, cargo_args: &[&str]) -> Child {
+    let mut args_vec = vec!["run"];
+    args_vec.extend(cargo_args);
+    lift_cargo(project_path, &args_vec)
+}
+
+fn ignite_boosters(project_path: &str, interval: f32, cargo_args: &[&str]) {
+    // Create event channel
+    let (tx, rx) = channel();
+
+    // Create filesystem watcher
+    let mut watcher = watcher(tx, Duration::from_millis((interval * 1000.0).round() as u64))
+        .unwrap_or_else(|e| panic!("Error creating filesystem watcher: {}", e));
+
+    // Watch project path
+    watcher.watch(project_path, RecursiveMode::Recursive)
+        .unwrap_or_else(|e| panic!("Error watching path: {}", e));
+
+    let mut cargo_p = relaunch_cargo(project_path, cargo_args);
+
+    loop {
+        match rx.recv() {
+            Ok(event) => {
+                let mut trigger = false;
+                match event {
+                    DebouncedEvent::NoticeWrite(path)
+                    | DebouncedEvent::NoticeRemove(path)
+                    | DebouncedEvent::Create(path)
+                    | DebouncedEvent::Write(path)
+                    | DebouncedEvent::Chmod(path)
+                    | DebouncedEvent::Remove(path) => {
+                        trigger = is_whitelisted(path);
+                    }
+                    DebouncedEvent::Rename(old, new) => {
+                        trigger = is_whitelisted(old)
+                            || is_whitelisted(new);
+                    }
+                    _ => {}
+                }
+                if trigger {
+                    match static_fire(project_path) {
+                        Ok(_) => {
+                            cargo_p.kill().unwrap_or_else(|e| panic!("Error terminating cargo: {}", e));
+                            println!("⚙ Stopped (cargo run)");
+                            cargo_p = relaunch_cargo(project_path, cargo_args);
+                        },
+                        Err(_) => {},
+                    }
+                }
+            }
+            Err(e) => println!("watch error: {:?}", e)
+        }
+    }
+}
+
+fn static_fire(project_path: &str) -> Result<(), &str> {
+    println!("⚙ Running cargo check");
+    let status = lift_cargo(project_path, &["check"]).wait()
+        .expect("Did not receive an exit status from cargo check");
+    println!("⚙ Done (cargo check)");
+    if !status.success() {
+        Err("Error during cargo check")
+    } else {
+        Ok(())
+    }
+}
+
+fn main() {
+    let matches = App::new("Rocket Launch")
+        .version("0.1.0")
+        .author("Tim Süberkrüb <dev@timsueberkrueb.io>")
+        .about("Watches your Cargo project for changes and \
+                relaunches Cargo when changes are detected.")
+        .arg(Arg::with_name("project")
+            .help("Path to your project (default: current directory)"))
+        .arg(Arg::with_name("seconds")
+            .long("interval")
+            .short("i")
+            .takes_value(true)
+            .help("Interval to check for filesystem changes"))
+        .arg(Arg::with_name("cargo_args")
+            .multiple(true)
+            .help("Cargo arguments (usage: <launcher-args> -- <cargo-args>)")
+        )
+        .get_matches();
+
+    let project_path: String = match matches.value_of("project") {
+        Some(path) => path.to_string(),
+        None => std::env::current_dir()
+            .expect("Error obtaining working directory")
+            .to_str().unwrap().to_string()
+    };
+    let interval: f32 = match matches.value_of("seconds") {
+        Some(interval_str) => interval_str.parse::<f32>().expect("Invalid interval"),
+        None => DEFAULT_CHECK_INTERVAL
+    };
+    let cargo_args: Vec<&str> = match matches.value_of("cargo_args") {
+        Some(args_str) => args_str.split(" ").collect(),
+        None => Vec::new()
+    };
+
+    let is_existing_dir = match metadata(&project_path) {
+        Ok(m) => m.is_dir(),
+        Err(_) => false
+    };
+    if !is_existing_dir {
+        panic!("\"{}\" is not a valid directory", &project_path);
+    }
+
+    match static_fire(&project_path) {
+        Err(e) => {
+            eprintln!("{}", e);
+            exit(1);
+        },
+        _ => {}
+    }
+
+    ignite_boosters(&project_path, interval, &cargo_args);
+}
